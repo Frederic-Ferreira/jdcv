9ef0890d79fad6dabe174c79ae6727db26f375d2
Loopsider <loopsider@MBP-de-Quentin.lan>
2023-06-02 22:20:51 +0200

    carousel done

--- app/components/Gems/Carousel.jsx
+++ app/components/Gems/Carousel.jsx
@@ -2,22 +2,94 @@ import { LeftOutlined, RightOutlined } from "@ant-design/icons"
 import ImageCard from "@app/components/Gems/ImageCard"
 import { imageCardStyles } from "@utils/image-card-style"
 import { v4 as uid } from "uuid"
+import { useEffect, useState, useRef } from "react"
 
 function Carousel(props) {
+  const [pixels, setPixels] = useState(0)
+  const [translate, setTranslate] = useState(0)
+  const carouselContainerRef = useRef(null)
+  const carouselRef = useRef(null)
+
+  const handleLeftClick = () => {
+    if (translate === 0) return
+    setTranslate(translate - 1)
+  }
+
+  const handleRightClick = () => {
+    const containerWidth = carouselContainerRef.current.offsetWidth
+    const carouselWidth = carouselRef.current.scrollWidth
+    const translatePixels = 300
+
+    if (translatePixels * translate + containerWidth < carouselWidth) {
+      setTranslate(translate + 1)
+    }
+  }
+
+  const handleResize = () => {
+    const containerWidth = carouselContainerRef.current.offsetWidth
+    const carouselWidth = carouselRef.current.scrollWidth
+    const translatePixels = 300
+
+    if (containerWidth + translatePixels * translate < carouselWidth) {
+      setPixels(translatePixels * translate)
+    } else {
+      const currentTranslate =
+        containerWidth + translatePixels * (translate - 1)
+      const leftPixels = carouselWidth - currentTranslate
+      setPixels(pixels + leftPixels)
+    }
+  }
+
+  useEffect(() => {
+    const containerWidth = carouselContainerRef.current.offsetWidth
+    const carouselWidth = carouselRef.current.scrollWidth
+    const translatePixels = 300
+
+    if (containerWidth + translatePixels * translate < carouselWidth) {
+      setPixels(translatePixels * translate)
+    } else {
+      const currentTranslate =
+        containerWidth + translatePixels * (translate - 1)
+      const leftPixels = carouselWidth - currentTranslate
+      setPixels(pixels + leftPixels)
+    }
+
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [translate])
+
   return (
     <div className="flex items-center gap-8 px-4">
-      <LeftOutlined className="hover:cursor-pointer text-4xl text-gray-300" />
-      <div className="flex gap-3 overflow-x-auto">
-        {imageCardStyles.map((style) => (
-          <ImageCard
-            key={uid()}
-            src={style.src}
-            category={style.category}
-            number={style.number}
-          />
-        ))}
+      <LeftOutlined
+        onClick={handleLeftClick}
+        className="hover:cursor-pointer text-4xl text-gray-300"
+      />
+      <div ref={carouselContainerRef} className="overflow-x-auto">
+        <div
+          ref={carouselRef}
+          style={{
+            transform: `translateX(-${pixels}px)`,
+            transition: "transform 0.5s ease-in-out",
+          }}
+          className="flex gap-3"
+        >
+          {imageCardStyles.map((style) => (
+            <ImageCard
+              key={uid()}
+              src={style.src}
+              category={style.category}
+              number={style.number}
+            />
+          ))}
+        </div>
       </div>
-      <RightOutlined className="hover:cursor-pointer text-4xl text-gray-300" />
+      <RightOutlined
+        onClick={handleRightClick}
+        className="hover:cursor-pointer text-4xl text-gray-300"
+      />
     </div>
   )
 }
--- app/components/Gems/ImageCard.jsx
+++ app/components/Gems/ImageCard.jsx
@@ -1,5 +1,6 @@
+"use client"
 import Image from "next/image"
-function ImageCard({ src, category, number }) {
+function ImageCard({ src, category, number, pixels }) {
   return (
     <div className="flex flex-col h-[275px] min-w-[180px] rounded-lg relative hover:cursor-pointer">
       <Image
@@ -9,8 +10,8 @@ function ImageCard({ src, category, number }) {
         alt={category}
         style={{ borderRadius: "20px", opacity: 0.6 }}
       />
-      <div className="absolute bottom-10 w-full h-1/3 flex flex-col gap-2 text-center">
-        <p className="text-sm">{category}</p>
+      <div className="absolute bottom-8 w-full h-1/3 flex flex-col gap-2 text-center font-normal ">
+        <p className="text-sm mt-auto">{category}</p>
         <p className="text-xs">+ de {number} pépites</p>
       </div>
     </div>

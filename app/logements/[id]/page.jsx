c3f1f6075f8a440b791be3ae86abbd7303097a67
Loopsider <loopsider@MacBook-Pro-de-Quentin.local>
2023-06-09 09:57:48 +0200

    housing detail advancements

--- app/logements/[id]/page.jsx
+++ app/logements/[id]/page.jsx
@@ -1,4 +1,6 @@
+"use client"
 import Image from "next/image"
+import { DownOutlined } from "@ant-design/icons"
 
 const logement = [
   "housing-1.png",
@@ -80,6 +82,99 @@ function Page(props) {
             </div>
           )
         })}
+      </section>
+      <section className="flex gap-8 px-20 mt-10">
+        <div className="flex flex-col gap-8 w-2/3">
+          <h2 className="gem-category-underline w-[125px] text-xl font-medium underline">
+            Présentation
+          </h2>
+          <p className="text-sm">
+            Bienvenue dans ce charmant chalet excentré en montagne à
+            Saint-Pierre-de-Chartreuse ! Niché au cœur des majestueuses Alpes
+            françaises, ce chalet pittoresque offre une échappée parfaite pour
+            les amoureux de la nature et les passionnés de montagne.
+          </p>
+          <p className="text-sm">
+            Dès votre arrivée, vous serez émerveillé par l'emplacement isolé de
+            cette retraite alpine. Situé en marge de la civilisation, vous
+            découvrirez un havre de paix entouré par des forêts verdoyantes, des
+            sommets imposants et une sérénité absolue. Vous vous sentirez
+            instantanément déconnecté du stress de la vie quotidienne,
+            permettant à votre esprit de se ressourcer dans ce cadre enchanteur.
+          </p>
+          <p className="text-sm">
+            Le chalet lui-même est un mélange parfait de confort moderne et de
+            charme rustique. Les murs en bois chaleureux et les poutres
+            apparentes créent une atmosphère accueillante et traditionnelle,
+            tandis que les équipements haut de gamme vous garantissent un séjour
+            des plus agréables. L'intérieur spacieux comprend un salon
+            confortable, une cuisine entièrement équipée et une salle à manger
+            conviviale, où vous pourrez partager des repas savoureux préparés
+            avec des produits locaux.
+          </p>
+          <p className="text-sm">
+            Alors, si vous recherchez une expérience authentique en montagne,
+            alliant la beauté des paysages à une ambiance de fête inoubliable,
+            faites confiance à Pierre et son chalet accueillant. Venez célébrer
+            la vie, la musique et les rencontres, et repartez avec des souvenirs
+            mémorables gravés dans votre cœur.
+          </p>
+        </div>
+        <div className="flex flex-col bg-[#F5F5F5] rounded-lg gap-8 w-1/3 py-10 px-4 shadow-xl">
+          <p>
+            à partir de <span className="text-lg font-bold">250e</span> / soirée
+          </p>
+          <div className="flex items-center justify-between gap-2">
+            <div className="flex flex-col gap-2">
+              <p className="text-sm">Dates</p>
+              <div className="flex items-center hover:cursor-pointer text-sm bg-white rounded-lg px-2 py-2 font-light justify-between gap-2 border border-1 border-gray-300">
+                <p className="text-gray-300">Choisir des dates</p>
+                <DownOutlined className="text-gray-300" />
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="text-sm">Nombre de personnes</p>
+              <div className="flex items-center hover:cursor-pointer text-sm bg-white rounded-lg px-2 py-2 font-light justify-between gap-2 border border-1 border-gray-300">
+                <p className="text-gray-300">12 personnes</p>
+                <DownOutlined className="text-gray-300" />
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-2 h-[100px] rounded-lg bg-white overflow-hidden">
+            <img
+              src="/images/housing/details/photos/housing-1.png"
+              width="33%"
+              height="100%"
+              alt="photo logement"
+            />
+            <div className="flex flex-col gap-2">
+              <p className="font-medium text-sm">Châlet convivial</p>
+              <div className="flex items-center gap-2">
+                <Image
+                  src="/images/housing/details/reservation/people.svg"
+                  width={15}
+                  height={15}
+                  alt="icon de personnes"
+                />
+                <p className="text-sm">15</p>
+                <Image
+                  src="/images/housing/details/reservation/room.svg"
+                  width={15}
+                  height={15}
+                  alt="icon de personnes"
+                />
+                <p className="text-sm">3</p>
+                <Image
+                  src="/images/housing/details/reservation/bed.svg"
+                  width={15}
+                  height={15}
+                  alt="icon de personnes"
+                />
+                <p className="text-sm">15</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   )
